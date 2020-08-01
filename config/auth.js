c929f3d4ad7c0ab900cdaacd309491fc7190ee9b
Daniel Bergholz <bergholz.daniel@gmail.com>
2020-08-01 12:06:07 -0300

    JWT now expires in 2 days

--- config/auth.js
+++ config/auth.js
@@ -73,7 +73,7 @@ module.exports = {
     password: 'password',
     options: {
       secret: Env.get('APP_KEY'),
-      expiresIn: 86400
+      expiresIn: 17280 // 172800 seconds = 2 days
     }
   },
 
