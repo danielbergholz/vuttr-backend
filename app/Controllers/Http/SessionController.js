fd7c27074b3d0ceff09bdc91f81c47b90259186d
Daniel Bergholz <bergholz.daniel@gmail.com>
2020-08-01 12:06:40 -0300

    Return user id, name and email on /session

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -1,12 +1,17 @@
 'use strict'
 
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const User = use('App/Models/User')
+
 class SessionController {
   async store ({ request, auth }) {
     const { email, password } = request.post()
 
     const token = await auth.attempt(email, password)
 
-    return token
+    const user = await User.findBy('email', email)
+
+    return { ...token, user }
   }
 }
 
