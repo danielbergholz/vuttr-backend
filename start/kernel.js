217b99dad9c2af0a47f48349206fa05eee303a2e
Daniel Bergholz <bergholz.daniel@gmail.com>
2020-08-01 12:05:44 -0300

    Add Static to serverMiddleware

--- start/kernel.js
+++ start/kernel.js
@@ -52,10 +52,8 @@ const namedMiddleware = {
 | control over request lifecycle.
 |
 */
-const serverMiddleware = Env.get('NODE_ENV') === 'development' ? [
+const serverMiddleware = [
   'Adonis/Middleware/Static',
-  'Adonis/Middleware/Cors'
-] : [
   'Adonis/Middleware/Cors'
 ]
 
