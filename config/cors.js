a0e931da33b375672c6c1b5e98664ea4cb84b530
Daniel Bergholz <bergholz.daniel@gmail.com>
2020-08-05 20:05:34 -0300

    Fix cors typo

--- config/cors.js
+++ config/cors.js
@@ -16,7 +16,7 @@ module.exports = {
   | Function - Receives the current origin and should return one of the above values.
   |
   */
-  origin: ['http://localhost:3000', 'https://vuttr-frontend.netlify.app/'],
+  origin: ['http://localhost:3000', 'https://vuttr-frontend.netlify.app'],
 
   /*
   |--------------------------------------------------------------------------
